7aa0212de69e46ae4a70f4b6cccc713e47acfa37
Adam Sitnik <adam.sitnik@microsoft.com>
2018-02-16 23:32:37 +0100

    json benchmarks

---
+++ Benchmarks/Serializers/DataGenerator.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Benchmarks.Serializers
+{
+    internal static class DataGenerator
+    {
+        internal static T Generate<T>()
+        {
+            if (typeof(T) == typeof(LoginViewModel))
+                return (T)(object)CreateLoginViewModel();
+            if (typeof(T) == typeof(Location))
+                return (T)(object)CreateLocation();
+            if (typeof(T) == typeof(IndexViewModel))
+                return (T)(object)CreateIndexViewModel();
+
+            throw new NotImplementedException();
+        }
+
+        private static LoginViewModel CreateLoginViewModel()
+            => new LoginViewModel
+            {
+                Email = "name.familyname@not.com",
+                Password = "abcdefgh123456!@",
+                RememberMe = true
+            };
+
+        private static Location CreateLocation()
+            => new Location
+            {
+                Id = 1234,
+                Address1 = "The Street Name",
+                Address2 = "20/11",
+                City = "The City",
+                State = "The State",
+                PostalCode = "abc-12",
+                Name = "Nonexisting",
+                PhoneNumber = "+0 11 222 333 44",
+                Country = "The Greatest"
+            };
+
+        private static IndexViewModel CreateIndexViewModel()
+            => new IndexViewModel
+            {
+                IsNewAccount = false,
+                FeaturedCampaign = new CampaignSummaryViewModel
+                {
+                    Description = "Very nice campaing",
+                    Headline = "The Headline",
+                    Id = 234235,
+                    OrganizationName = "The Company XYZ",
+                    ImageUrl = "https://www.dotnetfoundation.org/theme/img/carousel/foundation-diagram-content.png",
+                    Title = "Promoting Open Source"
+                },
+                ActiveOrUpcomingEvents = Enumerable.Repeat(
+                    new ActiveOrUpcomingEvent
+                    {
+                        Id = 10,
+                        CampaignManagedOrganizerName = "Name FamiltyName",
+                        CampaignName = "The very new campaing",
+                        Description = "The .NET Foundation works with Microsoft and the broader industry to increase the exposure of open source projects in the .NET community and the .NET Foundation. The .NET Foundation provides access to these resources to projects and looks to promote the activities of our communities.",
+                        EndDate = DateTime.UtcNow.AddYears(1),
+                        Name = "Just a name",
+                        ImageUrl = "https://www.dotnetfoundation.org/theme/img/carousel/foundation-diagram-content.png",
+                        StartDate = DateTime.UtcNow
+                    },
+                    count: 20).ToList()
+            };
+    }
+
+    // the view models come from a real world app called "AllReady"
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+
+    public class Location
+    {
+        public int Id { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Country { get; set; }
+    }
+
+    public class ActiveOrUpcomingCampaign
+    {
+        public int Id { get; set; }
+        public string ImageUrl { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTimeOffset StartDate { get; set; }
+        public DateTimeOffset EndDate { get; set; }
+    }
+
+    public class ActiveOrUpcomingEvent
+    {
+        public int Id { get; set; }
+        public string ImageUrl { get; set; }
+        public string Name { get; set; }
+        public string CampaignName { get; set; }
+        public string CampaignManagedOrganizerName { get; set; }
+        public string Description { get; set; }
+        public DateTimeOffset StartDate { get; set; }
+        public DateTimeOffset EndDate { get; set; }
+    }
+
+    public class CampaignSummaryViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public string OrganizationName { get; set; }
+        public string Headline { get; set; }
+    }
+
+    public class IndexViewModel
+    {
+        public List<ActiveOrUpcomingEvent> ActiveOrUpcomingEvents { get; set; }
+        public CampaignSummaryViewModel FeaturedCampaign { get; set; }
+        public bool IsNewAccount { get; set; }
+        public bool HasFeaturedCampaign => FeaturedCampaign != null;
+    }
+
+    public class MyEventsListerViewModel
+    {
+        public IEnumerable<MyEventsListerItem> CurrentEvents { get; set; } = new List<MyEventsListerItem>();
+        public IEnumerable<MyEventsListerItem> FutureEvents { get; set; } = new List<MyEventsListerItem>();
+        public IEnumerable<MyEventsListerItem> PastEvents { get; set; } = new List<MyEventsListerItem>();
+    }
+
+    public class MyEventsListerItem
+    {
+        public int EventId { get; set; }
+        public string EventName { get; set; }
+        public DateTimeOffset StartDate { get; set; }
+        public DateTimeOffset EndDate { get; set; }
+        public string TimeZone { get; set; }
+        public string Campaign { get; set; }
+        public string Organization { get; set; }
+        public int VolunteerCount { get; set; }
+
+        public List<MyEventsListerItemTask> Tasks { get; set; } = new List<MyEventsListerItemTask>();
+    }
+
+    public class MyEventsListerItemTask
+    {
+        public string Name { get; set; }
+        public DateTimeOffset? StartDate { get; set; }
+        public DateTimeOffset? EndDate { get; set; }
+
+        public string FormattedDate
+        {
+            get
+            {
+                if (!StartDate.HasValue || !EndDate.HasValue)
+                {
+                    return null;
+                }
+
+                var startDateString = string.Format("{0:g}", StartDate.Value);
+                var endDateString = string.Format("{0:g}", EndDate.Value);
+
+                return string.Format($"From {startDateString} to {endDateString}");
+            }
+        }
+    }
+}
