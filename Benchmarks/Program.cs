d0bc17de467acc3fac9b08603ef53a3f68a77186
Adam Sitnik <adam.sitnik@microsoft.com>
2018-02-23 18:49:55 +0100

    update to BDN which allows to run Mono vs .NET Core

--- Benchmarks/Program.cs
+++ Benchmarks/Program.cs
@@ -56,9 +56,10 @@ namespace Benchmarks
             Add(Job.Default.With(Runtime.Core).With(CsProjCoreToolchain.From(NetCoreAppSettings.NetCoreApp20)).AsBaseline().WithId("2.0"));
             Add(Job.Default.With(Runtime.Core).With(CsProjCoreToolchain.From(NetCoreAppSettings.NetCoreApp21)).WithId("2.1"));
 
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                Add(Job.Default.With(Runtime.Clr).With(CsProjClassicNetToolchain.Net47).WithId("4.7"));
-
+#if NET46
+            if (Type.GetType("Mono.Runtime") == null) // not a Mono
+                Add(Job.Default.With(Runtime.Clr).WithId("4.x"));
+#endif
             Add(Job.Default.With(Runtime.Mono).WithId("Mono"));
 
             Add(MemoryDiagnoser.Default);
@@ -67,11 +68,38 @@ namespace Benchmarks
             Add(DefaultConfig.Instance.GetLoggers().ToArray());
             Add(DefaultConfig.Instance.GetColumnProviders().ToArray());
 
-            Add(CsvMeasurementsExporter.Default);
-            Add(RPlotExporter.Default); // it produces nice plots but requires R to be installed
+            Add(new CsvMeasurementsExporter(CsvSeparator.Semicolon));
+            //Add(RPlotExporter.Default); // it produces nice plots but requires R to be installed
             Add(MarkdownExporter.GitHub);
             Add(HtmlExporter.Default);
             //Add(StatisticColumn.AllStatistics);
+        }
+    }
+
+    public class CustomPathsBenchmarkConfig : ManualConfig
+    {
+        public CustomPathsBenchmarkConfig(string dotnetCliPath, string monoPath)
+        {
+            Add(Job.Default.With(Runtime.Core).With(CsProjCoreToolchain.From(new NetCoreAppSettings("netcoreapp2.0", null, "2.0", dotnetCliPath))).AsBaseline().WithId("2.0"));
+            Add(Job.Default.With(Runtime.Core).With(CsProjCoreToolchain.From(new NetCoreAppSettings("netcoreapp2.1", null, "2.1", dotnetCliPath))).WithId("2.1"));
+
+#if NET46
+            if (Type.GetType("Mono.Runtime") == null) // not a Mono
+                Add(Job.Default.With(Runtime.Clr).WithId("4.x"));
+#endif
+
+            Add(Job.Default.With(new MonoRuntime("Mono", monoPath)).WithId("Mono"));
+
+            Add(MemoryDiagnoser.Default);
+
+            Add(DefaultConfig.Instance.GetValidators().ToArray());
+            Add(DefaultConfig.Instance.GetLoggers().ToArray());
+            Add(DefaultConfig.Instance.GetColumnProviders().ToArray());
+
+            Add(new CsvMeasurementsExporter(CsvSeparator.Semicolon));
+            Add(MarkdownExporter.GitHub);
+            Add(HtmlExporter.Default);
+            Add(StatisticColumn.AllStatistics);
         }
     }
 }
